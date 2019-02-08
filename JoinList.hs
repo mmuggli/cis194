8cbf8e31c3edbbfd006aa183224fc132d0e20e10
Martin Muggli <martin.muggli@gmail.com>
2019-02-08 08:57:59 -0700

    FIXME fix: prefer right subtree if left subtree has size 0 (i.e., Empty)

--- JoinList.hs
+++ JoinList.hs
@@ -29,12 +29,9 @@ _ !!? i | i < 0 = Nothing
 (x:xs) !!? i = xs !!? (i-1)                            
 
 
--- FIXME Likely fails intended use if Empty is a leaf node
--- though specification is vague.  Likely better to use
--- right subtree of Append if the size of the left is 0
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ 0 (Single m a) = Just a
-indexJ i (Append m jl1 jl2) | i <= (getSize (size m)) = if i <= (getSize (size (tag jl1)))
+indexJ i (Append m jl1 jl2) | i <= (getSize (size m)) = if i < (getSize (size (tag jl1)))
                                         then indexJ i jl1
                                         else indexJ (i - (getSize (size (tag jl1)))) jl2
 indexJ _ _ = Nothing    
